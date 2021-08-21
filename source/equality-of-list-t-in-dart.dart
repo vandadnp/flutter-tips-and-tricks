a95aa526c0ddd9ef5266b46a71163c0d00b81c11
Vandad Nahavandipoor <vandad.np@gmail.com>
2021-08-21 16:33:46 +0200

    Create equality-of-list-t-in-dart.dart

---
+++ source/equality-of-list-t-in-dart.dart
@@ -0,0 +1,35 @@
+// Want to support my work 🤝? https://buymeacoffee.com/vandad
+
+extension Equality<T> on List<T> {
+  bool isEqualTo(List<T> other) {
+    if (other.length != length) {
+      return false;
+    }
+    for (var i = 0; i < length; i++) {
+      if (other[i] != this[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+int ascendingComparator<T extends Comparable>(T lhs, T rhs) =>
+    lhs.compareTo(rhs);
+int descendingComparator<T extends Comparable>(T lhs, T rhs) =>
+    rhs.compareTo(lhs);
+
+extension Sorted<T extends Comparable> on List<T> {
+  List<T> sorted({bool descending = false}) => descending
+      ? ([...this]..sort(descendingComparator))
+      : ([...this]..sort(ascendingComparator));
+}
+
+void testIt() {
+  assert([1, 2, 3].isEqualTo([1, 2, 3]));
+  assert(![1, 2, 3].isEqualTo([1, 2, 2]));
+  assert([3, 1, 2].sorted().isEqualTo([1, 2, 3]));
+  assert(![3, 1, 2].sorted().isEqualTo([3, 1, 2]));
+  assert(['Foo', 'Bar', 'Baz'].isEqualTo(['Foo', 'Bar', 'Baz']));
+  assert(!['Foo', 'Bar', 'Baz'].isEqualTo(['foo', 'Bar', 'Baz']));
+}
