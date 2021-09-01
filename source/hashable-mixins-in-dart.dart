71bc4c423388e53a56cd2c7872b26916bf6d77a3
Vandad Nahavandipoor <vandad.np@gmail.com>
2021-09-01 08:25:56 +0200

    Create hashable-mixins-in-dart.dart

---
+++ source/hashable-mixins-in-dart.dart
@@ -0,0 +1,40 @@
+// Want to support my work 🤝? https://buymeacoffee.com/vandad
+
+enum PetType { cat, dog }
+
+mixin Pet {
+  String get name;
+  int get age;
+  PetType get type;
+  @override
+  String toString() => 'Pet ($type), name = $name, age = $age';
+  @override
+  int get hashCode => hashValues(name, age, type);
+  @override
+  bool operator ==(covariant Pet o) => o.hashCode == hashCode;
+}
+
+class Cat with Pet {
+  @override
+  final String name;
+  @override
+  final int age;
+  @override
+  final PetType type;
+  const Cat({required this.name, required this.age}) : type = PetType.cat;
+  @override
+  int get hashCode => hashValues(name, age, type);
+}
+
+void testIt() {
+  final cats = <Cat>{
+    Cat(name: 'Kitty 1', age: 2),
+    Cat(name: 'Kitty 2', age: 3),
+    Cat(name: 'Kitty 1', age: 2),
+  };
+  cats.forEach(print);
+  /* 👆🏻 prints the following:
+  Pet (PetType.cat), name = Kitty 1, age = 2
+  Pet (PetType.cat), name = Kitty 2, age = 3
+  */
+}
