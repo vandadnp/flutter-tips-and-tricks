94e0d68208313a0c2f7d84d374ec89708c0a1880
Vandad Nahavandipoor <vandad.np@gmail.com>
2021-11-26 10:21:54 +0100

    Create filter-on-streamlistt-in-dart.dart

---
+++ source/filter-on-streamlistt-in-dart.dart
@@ -0,0 +1,33 @@
+// Want to support my work 🤝? https://buymeacoffee.com/vandad
+
+import 'dart:developer' as devtools show log;
+
+extension Filter<T> on Stream<List<T>> {
+  Stream<List<T>> filter(bool Function(T) where) =>
+      map((items) => items.where(where).toList());
+}
+
+final Stream<List<int>> allNumbers = Stream.periodic(
+  const Duration(seconds: 1),
+  (value) => [for (var i = 0; i < value; i++) i],
+);
+
+bool isEven(num value) => value % 2 == 0;
+bool isOdd(num value) => value % 2 == 0;
+
+extension EvenOdd<E extends num> on Stream<List<E>> {
+  Stream<List<E>> get evenNumbers => filter(isEven);
+  Stream<List<E>> get oddNumbers => filter(isOdd);
+}
+
+void readEvenNumbers() async {
+  await for (final evenNumber in allNumbers.evenNumbers) {
+    devtools.log('All even numbers: $evenNumber');
+  }
+}
+
+void readOddNumbers() async {
+  await for (final oddNumber in allNumbers.oddNumbers) {
+    devtools.log('All odd number: $oddNumber');
+  }
+}
