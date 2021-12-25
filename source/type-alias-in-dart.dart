76c761e2d1be890ff7253019d3e063060c0846d9
Vandad Nahavandipoor <vandad.np@gmail.com>
2021-12-25 10:03:28 +0100

    Create type-alias-in-dart.dart

---
+++ source/type-alias-in-dart.dart
@@ -0,0 +1,15 @@
+// Want to support my work 🤝? https://buymeacoffee.com/vandad
+
+const Map<String, int> people1 = {
+  'foo': 20,
+  'bar': 30,
+  'baz': 25,
+};
+
+typedef Age = int;
+
+const Map<String, Age> people2 = {
+  'foo': 20,
+  'bar': 30,
+  'baz': 25,
+};
