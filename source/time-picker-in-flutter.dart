c28e9855e4ba30526188a362186ec819cb9c29d7
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-01-05 08:42:53 +0100

    Create time-picker-in-flutter.dart

---
+++ source/time-picker-in-flutter.dart
@@ -0,0 +1,31 @@
+// Want to support my work 🤝? https://buymeacoffee.com/vandad
+
+class HomePage extends StatelessWidget {
+  final timeOfDay = ValueNotifier<TimeOfDay?>(null);
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: timeOfDay,
+      builder: (context, value, child) {
+        final title = timeOfDay.value?.toString() ?? 'Time Picker in Flutter';
+        return Scaffold(
+          appBar: AppBar(title: Text(title)),
+          body: Center(
+            child: TextButton(
+              onPressed: () async {
+                await showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.now(),
+                  initialEntryMode: TimePickerEntryMode.input,
+                );
+              },
+              child: const Text('Please Pick a time'),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
