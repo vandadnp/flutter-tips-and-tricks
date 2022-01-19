d6a49394bea3c421a8db7fbaf9a1045b1da6b4d7
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-01-19 06:47:31 +0100

    Create localization-delegates-in-flutter.dart

---
+++ source/localization-delegates-in-flutter.dart
@@ -0,0 +1,25 @@
+// Free Flutter Course 💙 https://linktr.ee/vandadnp
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BlocProvider<AuthBloc>(
+        create: (context) => AuthBloc(FirebaseAuthProvider()),
+        child: const HomePage(),
+      ),
+      routes: {
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
+      },
+    ),
+  );
+}
