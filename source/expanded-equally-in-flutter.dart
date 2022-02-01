81189facc023f313004494508ba885e64412b0ac
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-02-01 07:38:04 +0100

    Create expanded-equally-in-flutter.dart

---
+++ source/expanded-equally-in-flutter.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    ),
+  );
+}
+
+extension ExpandEqually on Iterable<Widget> {
+  Iterable<Widget> expandedEqually() => map(
+        (w) => Expanded(
+          flex: 1,
+          child: w,
+        ),
+      );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                height: 200,
+                color: Colors.yellow,
+              ),
+              Container(
+                height: 200,
+                color: Colors.blue,
+              ),
+            ].expandedEqually().toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
