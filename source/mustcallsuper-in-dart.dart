3f8c940a102e160a445502410ec586f764e33005
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-02-05 06:56:38 +0100

    Create mustcallsuper-in-dart.dart

---
+++ source/mustcallsuper-in-dart.dart
@@ -0,0 +1,12 @@
+// Free Flutter Course 💙 https://linktr.ee/vandadnp
+// Want to support my work 🤝? https://buymeacoffee.com/vandad
+
+class Animal {
+  @mustCallSuper
+  void run() {}
+}
+
+class Dog extends Animal {
+  @override
+  void run() {}
+}
