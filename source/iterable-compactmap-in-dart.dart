58135fee265f3a3db82e8b79c0bf12b690a3f6f2
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-02-22 05:37:28 +0100

    Create iterable-compactmap-in-dart.dart

---
+++ source/iterable-compactmap-in-dart.dart
@@ -0,0 +1,23 @@
+// Free Flutter Course 💙 https://linktr.ee/vandadnp
+// Want to support my work 🤝? https://buymeacoffee.com/vandad
+
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+extension CompactMap<T> on Iterable<T> {
+  Iterable<T> compactMap<E>([
+    E? Function(T)? transform,
+  ]) =>
+      map(transform ?? (e) => e).where((e) => e != null).cast();
+}
+
+const list = ['Hello', null, 'World'];
+
+void testIt() {
+  list.log(); // [Hello, null, World]
+  list.compactMap().log(); // [Hello, World]
+  list.compactMap((e) => e?.toUpperCase()).log(); // [HELLO, WORLD]
+}
