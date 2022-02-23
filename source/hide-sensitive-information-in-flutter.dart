0226be8cc2f3663055af9953a7d67c5e9f5799eb
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-02-23 09:09:15 +0100

    Create hide-sensitive-information-in-flutter.dart

---
+++ source/hide-sensitive-information-in-flutter.dart
@@ -0,0 +1,50 @@
+// Free Flutter Course 💙 https://linktr.ee/vandadnp
+// Want to support my work 🤝? https://buymeacoffee.com/vandad
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends HookWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final state = useAppLifecycleState();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Opacity(
+          opacity: state == AppLifecycleState.resumed ? 1.0 : 0.0,
+          child: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 10,
+                  color: Colors.black.withAlpha(100),
+                  spreadRadius: 10,
+                ),
+              ],
+            ),
+            child: Image.asset('assets/card.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
