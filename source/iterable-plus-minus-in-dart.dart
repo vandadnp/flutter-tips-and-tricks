de758c11521500ac8f029d5cd9189f161041ed5f
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-03-05 06:32:59 +0100

    Create iterable-plus-minus-in-dart.dart

---
+++ source/iterable-plus-minus-in-dart.dart
@@ -0,0 +1,18 @@
+
+// Free Flutter Course 💙 https://linktr.ee/vandadnp
+// Want to support my work 🤝? https://buymeacoffee.com/vandad
+
+extension AddRemoveItems<T> on Iterable<T> {
+
+  Iterable<T> operator +(T other) => followedBy([other]);
+
+  Iterable<T> operator -(T other) => where((element) => element != other);
+
+}
+
+void testIt() {
+  final values = ['foo', 'bar']
+    .map((e) => e.toUpperCase()) + 'BAZ';
+  values.log(); // (FOO, BAR, BAZ)
+  (values - 'BAZ').log(); // (FOO, BAR)
+}
