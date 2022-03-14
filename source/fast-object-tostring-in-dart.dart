14c5bc60bb59c00af8d7f55c4f8144ce529485a4
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-03-14 04:58:02 +0100

    Create fast-object-tostring-in-dart.dart

---
+++ source/fast-object-tostring-in-dart.dart
@@ -0,0 +1,26 @@
+
+// Free Flutter Course 💙 https://linktr.ee/vandadnp
+// Want to support my work 🤝? https://buymeacoffee.com/vandad
+
+@immutable
+class AppState {
+  final bool isLoading;
+  final Object? loginError;
+  final String? loginHandle;
+  final Iterable<String>? fetchedNotes;
+
+  @override
+  String toString() => {
+        'isLoading': isLoading,
+        'loginError': loginError,
+        'loginHandle': loginHandle,
+        'fetchedNotes': fetchedNotes
+      }.toString();
+
+  const AppState({
+    required this.isLoading,
+    required this.loginError,
+    required this.loginHandle,
+    required this.fetchedNotes,
+  });
+}
