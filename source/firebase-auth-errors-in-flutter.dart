8fdec88d327f0cb6873728d94c821c5fb0bb2991
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-03-20 05:49:50 +0100

    Create firebase-auth-errors-in-flutter.dart

---
+++ source/firebase-auth-errors-in-flutter.dart
@@ -0,0 +1,32 @@
+// 🐦 Twitter:                  https://twitter.com/vandadnp
+// 🔵 LinkedIn:                 https://linkedin.com/in/vandadnp
+// 🎥 YouTube:                  https://youtube.com/c/vandadnp
+// 💙 Free Flutter Course:      https://linktr.ee/vandadnp
+// 🤝 Want to support my work?  https://buymeacoffee.com/vandad
+
+const authErrorMapping = {
+  'user-not-found': AuthErrorUserNotFound(),
+  'project-not-found': AuthErrorProjectNotFound(),
+};
+
+@immutable
+abstract class AuthError {
+  factory AuthError.from(FirebaseAuthException exception) =>
+      authErrorMapping[exception.code.toLowerCase().trim] ??
+      const AuthErrorUnknown();
+}
+
+@immutable
+class AuthErrorUnknown implements AuthError {
+  const AuthErrorUnknown();
+}
+
+@immutable
+class AuthErrorUserNotFound implements AuthError {
+  const AuthErrorUserNotFound();
+}
+
+@immutable
+class AuthErrorProjectNotFound implements AuthError {
+  const AuthErrorProjectNotFound();
+}
