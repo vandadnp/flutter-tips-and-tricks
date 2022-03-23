edd9a74e6a734b3ba5199b20a919a09e1c183137
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-03-23 05:13:06 +0100

    Create maplist-in-flutter.dart

---
+++ source/maplist-in-flutter.dart
@@ -0,0 +1,10 @@
+// 🐦 Twitter:                  https://twitter.com/vandadnp
+// 🔵 LinkedIn:                 https://linkedin.com/in/vandadnp
+// 🎥 YouTube:                  https://youtube.com/c/vandadnp
+// 💙 Free Flutter Course:      https://linktr.ee/vandadnp
+// 🤝 Want to support my work?  https://buymeacoffee.com/vandad
+
+extension MapToList<T> on Iterable<T> {
+  List<E> mapList<E>(E Function(T) toElement) => 
+    map(toElement).toList();
+}
