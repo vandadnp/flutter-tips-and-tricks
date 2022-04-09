2ed2ffdea59e9bd0d258d0a189c3e48bfaee5f9f
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-04-09 06:12:44 +0200

    Create hide-keyword-in-dart.dart

---
+++ source/hide-keyword-in-dart.dart
@@ -0,0 +1,13 @@
+// 🐦 Twitter                   https://twitter.com/vandadnp
+// 🔵 LinkedIn                  https://linkedin.com/in/vandadnp
+// 🎥 YouTube                   https://youtube.com/c/vandadnp
+// 💙 Free Flutter Course       https://linktr.ee/vandadnp
+// 📦 11+ Hours Bloc Course     https://youtu.be/Mn254cnduOY
+// 🤝 Want to support my work?  https://buymeacoffee.com/vandad
+
+import 'package:mobx/mobx.dart';
+import 'package:flutter_hooks/flutter_hooks.dart' hide Store;
+
+abstract class Reminder with Store {
+  // write your code here...
+}
