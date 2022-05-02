7ea37572f1e9161c39c875bfde03a2304e2337c8
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-05-02 06:24:35 +0200

    Create stream-drain-in-flutter.dart

---
+++ source/stream-drain-in-flutter.dart
@@ -0,0 +1,71 @@
+// 🐦 Twitter                   https://twitter.com/vandadnp
+// 🔵 LinkedIn                  https://linkedin.com/in/vandadnp
+// 🎥 YouTube                   https://youtube.com/c/vandadnp
+// 💙 Free Flutter Course       https://linktr.ee/vandadnp
+// 📦 11+ Hours Bloc Course     https://youtu.be/Mn254cnduOY
+// 🔶 7+ Hours MobX Course      https://youtu.be/7Od55PBxYkI
+// 🤝 Want to support my work?  https://buymeacoffee.com/vandad
+
+import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+Stream<DateTime> dateTimesWithoutError() async* {
+  yield DateTime.now();
+  yield DateTime.now();
+}
+
+Stream<DateTime> dateTimesWithError() async* {
+  yield DateTime.now();
+  yield DateTime.now();
+  throw Exception('An exception occurred');
+}
+
+void testIt() async {
+  try {
+    (await dateTimesWithoutError().drain('Success')).log();
+    (await dateTimesWithError().drain('Success')).log();
+  } on Exception catch (e) {
+    e.log();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    testIt();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+    );
+  }
+}
