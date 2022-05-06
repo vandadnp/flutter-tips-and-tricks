15cf6b650c57298498b9330b53513f4c9085f598
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-05-06 05:57:17 +0200

    Create functions-as-first-class-citizens-in-flutter.dart

---
+++ source/functions-as-first-class-citizens-in-flutter.dart
@@ -0,0 +1,59 @@
+// 🐦 Twitter                   https://twitter.com/vandadnp
+// 🔵 LinkedIn                  https://linkedin.com/in/vandadnp
+// 🎥 YouTube                   https://youtube.com/c/vandadnp
+// 💙 Free Flutter Course       https://linktr.ee/vandadnp
+// 📦 11+ Hours Bloc Course     https://youtu.be/Mn254cnduOY
+// 🔶 7+ Hours MobX Course      https://youtu.be/7Od55PBxYkI
+// 🤝 Want to support my work?  https://buymeacoffee.com/vandad
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(
+        goBack: () {
+          // empty for now
+        },
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final VoidCallback goBack;
+  const HomePage({
+    Key? key,
+    required this.goBack,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        leading: IconButton(
+          onPressed: goBack,
+          icon: const Icon(
+            Icons.close,
+          ),
+        ),
+      ),
+    );
+  }
+}
