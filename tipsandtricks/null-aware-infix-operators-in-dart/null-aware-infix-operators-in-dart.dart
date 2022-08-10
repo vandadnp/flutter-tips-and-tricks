c950f97e238410d52b1a84a814b9e7f46659585a
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-08-10 06:27:02 +0200

    Create null-aware-infix-operators-in-dart.dart

---
+++ tipsandtricks/null-aware-infix-operators-in-dart/null-aware-infix-operators-in-dart.dart
@@ -0,0 +1,34 @@
+// 🐦 Twitter                   https://twitter.com/vandadnp
+// 🔵 LinkedIn                  https://linkedin.com/in/vandadnp
+// 🎥 YouTube                   https://youtube.com/c/vandadnp
+// 💙 Free Flutter Course       https://linktr.ee/vandadnp
+// 📦 11+ Hours Bloc Course     https://youtu.be/Mn254cnduOY
+// 🔶 7+ Hours MobX Course      https://youtu.be/7Od55PBxYkI
+// 🦄 8+ Hours RxSwift Coursde  https://youtu.be/xBFWMYmm9ro
+// 🤝 Want to support my work?  https://buymeacoffee.com/vandad
+
+import 'dart:developer' as devtools show log;
+
+extension Log on Object? {
+  void log() => devtools.log(toString());
+}
+
+extension OptionalInfixAddition<T extends num> on T? {
+  T? operator +(T? other) {
+    final shadow = this;
+    if (shadow != null) {
+      return shadow + (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
+}
+
+void testIt() {
+  int? age;
+  age++; // age = null
+  age += 1; // age = null
+  age = 2; // age = 2
+  age++; // age = 3
+  age.log();
+}
