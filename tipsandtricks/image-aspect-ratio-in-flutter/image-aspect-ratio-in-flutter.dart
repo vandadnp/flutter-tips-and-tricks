ce12dfb3084a76cb673a2825d626390e8fdde7a9
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-09-05 06:58:34 +0200

    Create image-aspect-ratio-in-flutter.dart

---
+++ tipsandtricks/image-aspect-ratio-in-flutter/image-aspect-ratio-in-flutter.dart
@@ -0,0 +1,47 @@
+// 🐦 Twitter                   https://twitter.com/vandadnp
+// 🔵 LinkedIn                  https://linkedin.com/in/vandadnp
+// 🎥 YouTube                   https://youtube.com/c/vandadnp
+// 💙 Free Flutter Course       https://linktr.ee/vandadnp
+// 📦 11+ Hours Bloc Course     https://youtu.be/Mn254cnduOY
+// 🔶 7+ Hours MobX Course      https://youtu.be/7Od55PBxYkI
+// 🦄 8+ Hours RxSwift Coursde  https://youtu.be/xBFWMYmm9ro
+// 🤝 Want to support my work?  https://buymeacoffee.com/vandad
+
+import 'dart:io' show File;
+import 'dart:typed_data' show Uint8List;
+import 'dart:async' show Completer;
+import 'package:flutter/material.dart' as material
+    show Image, ImageConfiguration, ImageStreamListener;
+
+extension GetImageAspectRatio on material.Image {
+  Future<double> getAspectRatio() {
+    final completer = Completer<double>();
+    image.resolve(const material.ImageConfiguration()).addListener(
+      material.ImageStreamListener(
+        (imageInfo, synchronousCall) {
+          final aspectRatio = imageInfo.image.width / imageInfo.image.height;
+          imageInfo.image.dispose();
+          completer.complete(aspectRatio);
+        },
+      ),
+    );
+    return completer.future;
+  }
+}
+
+extension GetImageDataAspectRatio on Uint8List {
+  Future<double> getAspectRatio() async {
+    final image = material.Image.memory(this);
+    return image.getAspectRatio();
+  }
+}
+
+typedef FilePath = String;
+
+extension GetImageFileAspectRatio on FilePath {
+  Future<double> getAspectRatio() {
+    final file = File(this);
+    final image = material.Image.file(file);
+    return image.getAspectRatio();
+  }
+}
