c1517b4dae164f87bd216d5976ba69630606472d
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-09-10 07:12:23 +0200

    Create rich-text-construction-in-flutter.dart

---
+++ tipsandtricks/rich-text-construction-in-flutter/rich-text-construction-in-flutter.dart
@@ -0,0 +1,150 @@
+// 🐦 Twitter                   https://twitter.com/vandadnp
+// 🔵 LinkedIn                  https://linkedin.com/in/vandadnp
+// 🎥 YouTube                   https://youtube.com/c/vandadnp
+// 💙 Free Flutter Course       https://linktr.ee/vandadnp
+// 📦 11+ Hours Bloc Course     https://youtu.be/Mn254cnduOY
+// 🔶 7+ Hours MobX Course      https://youtu.be/7Od55PBxYkI
+// 🦄 8+ Hours RxSwift Coursde  https://youtu.be/xBFWMYmm9ro
+// 🤝 Want to support my work?  https://buymeacoffee.com/vandad
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+@immutable
+class BaseText {
+  final String text;
+  final TextStyle? style;
+  const BaseText({
+    required this.text,
+    this.style,
+  });
+
+  factory BaseText.plain({
+    required String text,
+    TextStyle? style = const TextStyle(),
+  }) =>
+      BaseText(
+        text: text,
+        style: style,
+      );
+
+  factory BaseText.link({
+    required String text,
+    required VoidCallback onTapped,
+    TextStyle? style = const TextStyle(
+      color: Colors.blue,
+      decoration: TextDecoration.underline,
+    ),
+  }) =>
+      LinkText(
+        text: text,
+        onTapped: onTapped,
+        style: style,
+      );
+}
+
+@immutable
+class LinkText extends BaseText {
+  final VoidCallback onTapped;
+  const LinkText({
+    required super.text,
+    required this.onTapped,
+    super.style,
+  });
+}
+
+class RichTextWidget extends StatelessWidget {
+  final TextStyle? styleForAll;
+  final Iterable<BaseText> texts;
+  const RichTextWidget({
+    super.key,
+    required this.texts,
+    this.styleForAll,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+          children: texts.map(
+        (baseText) {
+          if (baseText is LinkText) {
+            return TextSpan(
+              text: baseText.text,
+              style: styleForAll?.merge(baseText.style),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  baseText.onTapped();
+                },
+            );
+          } else {
+            return TextSpan(
+              text: baseText.text,
+              style: styleForAll?.merge(baseText.style),
+            );
+          }
+        },
+      ).toList()),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: RichTextWidget(
+          styleForAll: Theme.of(context).textTheme.headline2,
+          texts: [
+            BaseText.plain(
+              text: 'Welcome to my blog at ',
+            ),
+            BaseText.link(
+              text: 'https://vandad.sh',
+              onTapped: () {
+                'Tapped'.log();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
