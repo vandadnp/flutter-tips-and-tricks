26debce29e03f91222d721e82ea0b7db6f9426a8
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-10-12 06:55:55 +0200

    Create reusable-lottie-animations-in-flutter.dart

---
+++ tipsandtricks/reusable-lottie-animations-in-flutter/reusable-lottie-animations-in-flutter.dart
@@ -0,0 +1,90 @@
+// 🐦 Twitter                   https://twitter.com/vandadnp
+// 🔵 LinkedIn                  https://linkedin.com/in/vandadnp
+// 🎥 YouTube                   https://youtube.com/c/vandadnp
+// 💙 Free Flutter Course       https://linktr.ee/vandadnp
+// 📦 11+ Hours Bloc Course     https://youtu.be/Mn254cnduOY
+// 🔶 7+ Hours MobX Course      https://youtu.be/7Od55PBxYkI
+// 🦄 8+ Hours RxSwift Coursde  https://youtu.be/xBFWMYmm9ro
+// 🤝 Want to support my work?  https://buymeacoffee.com/vandad
+
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      darkTheme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+enum LottieAnimation {
+  dataNotFound(name: 'data_not_found'),
+  empty(name: 'empty'),
+  loading(name: 'loading'),
+  error(name: 'error'),
+  smallError(name: 'small_error');
+
+  final String name;
+  const LottieAnimation({
+    required this.name,
+  });
+}
+
+class LottieAnimationView extends StatelessWidget {
+  final LottieAnimation animation;
+  final bool repeat;
+  final bool reverse;
+  const LottieAnimationView({
+    super.key,
+    required this.animation,
+    this.repeat = true,
+    this.reverse = false,
+  });
+
+  @override
+  Widget build(BuildContext context) => Lottie.asset(
+        animation.fullPath,
+        reverse: reverse,
+        repeat: repeat,
+      );
+}
+
+extension GetFullPath on LottieAnimation {
+  String get fullPath => 'assets/animations/$name.json';
+}
+
+class EmptyContentsAnimationView extends LottieAnimationView {
+  const EmptyContentsAnimationView({super.key})
+      : super(
+          animation: LottieAnimation.empty,
+        );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: const EmptyContentsAnimationView(),
+    );
+  }
+}
