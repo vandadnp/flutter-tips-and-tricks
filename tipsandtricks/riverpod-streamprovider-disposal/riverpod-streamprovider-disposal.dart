1eb3314e654a0e725cd06e4048dd5527d450b023
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-10-30 07:31:56 +0100

    Create riverpod-streamprovider-disposal.dart

---
+++ tipsandtricks/riverpod-streamprovider-disposal/riverpod-streamprovider-disposal.dart
@@ -0,0 +1,44 @@
+// 🐦 Twitter                   https://twitter.com/vandadnp
+// 🔵 LinkedIn                  https://linkedin.com/in/vandadnp
+// 🎥 YouTube                   https://youtube.com/c/vandadnp
+// 💙 Free Flutter Course       https://linktr.ee/vandadnp
+// 📦 11+ Hours Bloc Course     https://youtu.be/Mn254cnduOY
+// 🔶 7+ Hours MobX Course      https://youtu.be/7Od55PBxYkI
+// 🦄 8+ Hours RxSwift Coursde  https://youtu.be/xBFWMYmm9ro
+// 🤝 Want to support my work?  https://buymeacoffee.com/vandad
+
+final userInfoModelProvider =
+    StreamProvider.family.autoDispose<UserInfoModel, UserId>(
+  (ref, UserId userId) {
+    final controller = StreamController<UserInfoModel>();
+
+    final sub = FirebaseFirestore.instance
+        .collection(
+          FirebaseCollectionName.users,
+        )
+        .where(
+          FirebaseFieldName.userId,
+          isEqualTo: userId,
+        )
+        .limit(1)
+        .snapshots()
+        .listen((snapshot) {
+      if (snapshot.docs.isNotEmpty) {
+        final doc = snapshot.docs.first;
+        final json = doc.data();
+        final userInfoModel = UserInfoModel.fromJson(
+          json,
+          userId: userId,
+        );
+        controller.add(userInfoModel);
+      }
+    });
+
+    ref.onDispose(() {
+      sub.cancel();
+      controller.close();
+    });
+
+    return controller.stream;
+  },
+);
